a0b0ab629a908bee2ccfce842783a8e4172d52cf
Lucas Accurcio <lucas.accurcio@gmail.com>
2024-12-14 18:07:09 -0300

    refactor: remove variables and add skip step when S3 exist

--- iac/variables.tf
+++ iac/variables.tf
@@ -11,11 +11,6 @@ variable "db_postgres_database" {
   type        = string
 }
 
-variable "state_bucket" {
-  description = "S3 bucket with versioning enabled to store Terraform state"
-  type        = string
-}
-
 variable "project_name" {
   description = "Project name"
   type        = string
@@ -25,9 +20,4 @@ variable "region_default" {
   type        = string
   description = "AWS Region"
   default     = "us-east-1"
-}
-
-variable "aws_account_id" {
-  description = "ID da conta AWS Lab Academy"
-  type        = string
 }
