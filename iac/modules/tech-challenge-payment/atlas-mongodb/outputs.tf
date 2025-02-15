bec31144d04dc169f92db5cce0b299e7a3ef06ae
Winderson Santos <winderson.wjbs@gmail.com>
2025-02-15 20:05:12 -0300

    fix: altera url de conexao mongo db

--- iac/modules/tech-challenge-payment/atlas-mongodb/outputs.tf
+++ iac/modules/tech-challenge-payment/atlas-mongodb/outputs.tf
@@ -1,4 +1,6 @@
-output "atlas_cluster_connection_string" { value = mongodbatlas_cluster.atlas-cluster.srv_address }
+output "atlas_cluster_connection_string" { 
+  value = "mongodb+srv://${mongodbatlas_database_user.db-user.username}:${random_password.db-user-password.result}@${mongodbatlas_cluster.atlas-cluster.name}.mongodb.net?retryWrites=true&w=majority"
+}
 output "ip_access_list" { value = mongodbatlas_project_ip_access_list.ip.ip_address }
 output "project_name" { value = mongodbatlas_project.atlas-project.name }
 output "username" { value = mongodbatlas_database_user.db-user.username }
@@ -6,6 +8,3 @@ output "user_password" {
   sensitive = true
   value     = mongodbatlas_database_user.db-user.password
 }
-# output "privatelink_connection_string" {
-#   value = lookup(mongodbatlas_advanced_cluster.atlas-cluster.connection_strings[0].aws_private_link_srv, aws_vpc_endpoint.ptfe_service.id)
-# }
